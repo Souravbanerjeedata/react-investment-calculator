a4d1a10aa9576926c702447ac95528d2aa7b6a8e
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2025-05-11 13:42:24 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import Header from "./Components/Header.jsx";
+import UserInput from "./Components/UserInput.jsx";
+import Results from "./Components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const validInput = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {validInput ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
